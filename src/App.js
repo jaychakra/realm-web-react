769dba279eb42c348cd6f2bf3801a12568ea8f27
Jay Chakra <jaychakra.jc@gmail.com>
2021-02-23 05:30:21 +0530

    Added code with working watcher

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import "./styles.css";
 import * as Realm from "realm-web";
 
 // Connect to your MongoDB Realm app
-const REALM_APP_ID = "<Your App ID>"; // e.g. myapp-abcde
+const REALM_APP_ID = "websdk_watch-jxuke"; // e.g. myapp-abcde
 const app = new Realm.App({ id: REALM_APP_ID });
+// const mongodb = app.services.mongodb("mongodb-atlas");
+// console.log(app);
 
-// Create a component that displays the given user's details
+
+// const collection = mongodb.db("websdk_watch").collection("demo");
+// Create a component that displays the given user's detail
 function UserDetail({ user }) {
   return (
     <div>
-      <h1>Logged in with anonymous id: {user.id}</h1>
+      <h1>Logged in with anonymous id: {JSON.stringify(user.id)}</h1>
     </div>
   );
 }
@@ -20,26 +24,89 @@ function UserDetail({ user }) {
 // Create a component that lets an anonymous user log in
 function Login({ setUser }) {
   const loginAnonymous = async () => {
-    const user = await app.logIn(Realm.Credentials.anonymous());
+    const user = await app.logIn(Realm.Credentials.emailPassword("root@google.com", "123456"));
     setUser(user);
   };
   return <button onClick={loginAnonymous}>Log In</button>;
 }
 
-const App = () => {
-  // Keep the logged in Realm user in local state. This lets the app re-render
-  // whenever the current user changes (e.g. logs in or logs out).
-  const [user, setUser] = React.useState(app.currentUser);
 
-  // If a user is logged in, show their details.
-  // Otherwise, show the login screen.
-  return (
-    <div className="App">
-      <div className="App-header">
-        {user ? <UserDetail user={user} /> : <Login setUser={setUser} />}
-      </div>
-    </div>
-  );
-};
+const View = ({data}) => {
+    console.log(data);
+    return <li>{data}</li>
+}
+const watcher = async (collection) => {
+    for await (const change of collection.watch()) {
+        console.log(change);
+        // const {operationType} = change;
+        // switch (operationType) {
+        //     case "insert": {
+        //         const {documentKey, fullDocument} = change;
+        //         console.log(`new document: ${documentKey}`, fullDocument);
+        //         break;
+        //     }
+        //     case "update": {
+        //         const {documentKey, fullDocument} = change;
+        //         console.log(`updated document: ${documentKey}`, fullDocument);
+        //         break;
+        //     }
+        //     case "replace": {
+        //         const {documentKey, fullDocument} = change;
+        //         console.log(`replaced document: ${documentKey}`, fullDocument);
+        //         break;
+        //     }
+        //     case "delete": {
+        //         const {documentKey} = change;
+        //         console.log(`deleted document: ${documentKey}`);
+        //         break;
+        //     }
+        // }
+    }
+}
+
+class App extends React.Component {
+
+    state = {
+        user: null,
+        data: [],
+        isLoggedIn: false,
+        collection: null
+    }
+
+    componentDidMount() {
+        app.logIn(Realm.Credentials.emailPassword("root@google.com", "123456"))
+            .then((user) => {
+                this.setUser(user);
+            })
+            .then(() => {
+                const mongodb = app.currentUser.mongoClient("mongodb-atlas");
+                const collection = mongodb.db("websdk_watch").collection("demo");
+                this.setState({collection});
+                collection.find().then(data => this.setState({data}))
+                watcher(collection);
+            });
+    }
+
+    setUser = (user) => {
+        this.setState({user, isLoggedIn: true});
+    }
+
+    render() {
+        const {user, isLoggedIn, data} = this.state;
+        // console.log("data", data);
+        let string = data.map(d => <View data={`${d.name} on partition ${d._partition}`} />);
+        return (
+            <div className="App">
+                <div className="App-header">
+                    {isLoggedIn ? <UserDetail user={user}  /> : <Login setUser={this.setUser} />}
+
+                </div>
+                <div>
+                    {string}
+                </div>
+            </div>
+        );
+    }
+}
 
 export default App;
